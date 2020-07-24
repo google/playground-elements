160841cae82481c757bebd6319d59c3e51651f46
Justin Fagnani <justinfagnani@google.com>
2020-07-23 18:27:32 -0700

    Delete commented out code

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -8,41 +8,6 @@ declare global {
   interface ImportMeta {
     url: string;
   }
-
-  // interface FetchEvent extends Event {
-  //   readonly request?: Request;
-  //   respondWith?: (response: Response | Promise<Response>) => void;
-  //   readonly clientId: string;
-  // }
-
-  // interface ActivateEvent extends Event {
-  //   waitUntil?: (waitable?: Promise<any>) => void;
-  // }
-
-  // type clientType = 'window' | 'worker' | 'sharedworker';
-
-  // interface Client {
-  //   postMessage(message: any, transfer?: Transferable[]): void;
-  //   postMessage(message: any, options?: { transfer?: any[] }): void;
-  //   readonly id: string;
-  //   readonly type: clientType;
-  //   readonly url: string;
-  //   readonly navigate: (url: string) => void;
-  // }
-
-  // interface Clients {
-  //   claim: () => Promise<void>;
-  //   get: (id: string) => Promise<Client | undefined>;
-  //   matchAll(options?: {
-  //     includeUntrontrolled?: boolean;
-  //     type?: 'window' | 'worker' | 'sharedworker' | 'all';
-  //   }): Promise<Client[]>;
-  // }
-
-  // interface ServiceWorkerGlobalScope extends Window {
-  //   registration: ServiceWorkerRegistration;
-  //   clients: Clients;
-  // }
 }
 
 // TODO (justinfagnani): this is temporary while we move the SwController
