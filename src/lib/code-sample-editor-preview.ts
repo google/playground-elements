448028c3e25ff48e7cba4119e16fc4389010168b
Justin Fagnani <justinfagnani@google.com>
2020-07-23 18:55:37 -0700

    Update service worker logic, add MWC components

---
+++ src/lib/code-sample-editor-preview.ts
@@ -0,0 +1,110 @@
+/**
+ * @license
+ * Copyright (c) 2019 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at
+ * http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at
+ * http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+import {
+  customElement,
+  LitElement,
+  html,
+  css,
+  property,
+  query,
+} from 'lit-element';
+import { ifDefined } from 'lit-html/directives/if-defined.js';
+import '@material/mwc-icon-button';
+import '@material/mwc-textfield';
+
+@customElement('code-sample-editor-preview')
+export class CodeSampleEditorPreviewElement extends LitElement {
+  static styles = css`
+    * {
+      box-sizing: border-box;
+    }
+
+    :host {
+      display: flex;
+      flex-direction: column;
+      width: 400px;
+    }
+
+    #toolbar {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      flex: 0 0 36px;
+      color: #444;
+      background: #f0f0f0;
+      border-bottom: 1px solid #ddd;
+      padding: 0 4px;
+      --mdc-typography-button-font-size: 0.75rem;
+      font-size: var(--mdc-typography-button-font-size, 0.75rem);
+    }
+
+    #toolbar mwc-icon-button {
+      cursor: default !important;
+      --mdc-icon-button-size: 32px;
+      --mdc-icon-size: 20px;
+    }
+
+    #location {
+      height: 24px;
+      line-height: 18px;
+      /* border-radius: 4px; */
+      padding: 4px;
+      /* border: solid 1px #ccc;
+      outline: none; */
+      font-family: var(
+        --mdc-typography-button-font-family,
+        var(--mdc-typography-font-family, Roboto, sans-serif)
+      );
+    }
+
+    iframe {
+      flex: 1;
+      width: 100%;
+      border: none;
+    }
+  `;
+
+  @property()
+  src?: string;
+
+  @property()
+  location?: string;
+
+  @query('iframe')
+  private _iframe!: HTMLIFrameElement;
+
+  render() {
+    return html`
+      <div id="toolbar">
+        <mwc-icon-button
+          icon="refresh"
+          @click=${this._onReloadClick}
+        ></mwc-icon-button>
+        <div id="location">${this.location}</div>
+      </div>
+      <iframe src="${ifDefined(this.src)}"></iframe>
+    `;
+  }
+
+  reload() {
+    if (this._iframe.contentWindow) {
+      this._iframe.contentWindow.location.reload();
+    }
+  }
+
+  private _onReloadClick() {
+    this.dispatchEvent(new Event('reload'));
+  }
+}
--- src/service-worker/service-worker.ts
+++ src/service-worker/service-worker.ts
@@ -1,37 +1,57 @@
-import {Message, FileRecord} from '../shared/types.js';
-import {endWithSlash} from '../shared/util.js';
-import {expose} from 'comlink';
-import {MESSAGE_TYPES} from '../shared/constants.js';
+/**
+ * @license
+ * Copyright (c) 2019 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at
+ * http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at
+ * http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
 
-const swScope = (self as unknown) as ServiceWorkerGlobalScope;
+import {
+  HANDSHAKE_RECEIVED,
+  ESTABLISH_HANDSHAKE,
+  ServiceWorkerAPI,
+  FileAPI,
+} from '../shared/worker-api.js';
+import { endWithSlash } from '../shared/util.js';
+import { expose } from 'comlink';
 
-const recieveMessageChannelHandshake = (e: MessageEvent) => {
-  const ports = e.ports;
-  if (ports && ports[0]) {
-    const port = ports[0];
-    port.start();
-    const handshakeReceivedMessage: Message = {
-      type: MESSAGE_TYPES.HANDSHAKE_RECEIVED,
-    };
-    expose(SwController, port);
-    port.postMessage(handshakeReceivedMessage);
-  }
+declare var self: ServiceWorkerGlobalScope;
+
+/**
+ * API exposed to the UI thread via Comlink. The static methods on this class
+ * become instance methods on SwControllerAPI.
+ */
+const workerAPI: ServiceWorkerAPI = {
+  setFileAPI(fileAPI: FileAPI, sessionID: string) {
+    fileAPIs.set(sessionID, fileAPI);
+  },
 };
 
-interface ResponseParams {
-  content: string;
-  init: ResponseInit;
-}
-
-let fileResponseMap = new Map<string, Map<string, ResponseParams>>();
+const fileAPIs = new Map<string, FileAPI>();
+const getFile = async (e: FetchEvent, path: string, sessionId: string) => {
+  const fileAPI = fileAPIs.get(sessionId);
+  if (fileAPI) {
+    const file = await fileAPI.getFile(path);
+    if (file) {
+      const headers = file.contentType
+        ? { 'Content-Type': file.contentType }
+        : undefined;
+      return new Response(file.content, { headers });
+    }
+  }
+  return fetch(e.request);
+};
 
 const onFetch = (e: FetchEvent) => {
-  if (!e.request || !e.request.url || !e.respondWith) {
-    return;
-  }
   const url = new URL(e.request.url);
   const href = url.href;
-  const scope = endWithSlash(swScope.registration.scope);
+  const scope = endWithSlash(self.registration.scope);
   if (href.startsWith(scope)) {
     const fullPath = href.substring(scope.length);
     const fullPathParts = fullPath.split('/');
@@ -40,74 +60,33 @@ const onFetch = (e: FetchEvent) => {
       return;
     }
     const path = fullPathParts.join('/');
-
-    const sessionMap = fileResponseMap.get(sessionId);
-    const responseRecord = sessionMap ? sessionMap.get(path) : null;
-
-    if (responseRecord) {
-      const response = new Response(
-        responseRecord.content,
-        responseRecord.init
-      );
-      e.respondWith(response);
-    }
+    e.respondWith(getFile(e, path, sessionId));
   }
 };
 
-self.addEventListener('fetch', onFetch as EventListenerOrEventListenerObject);
+const onInstall = (_e: Event) => {
+  // Force this service worker to become the active service worker, in case
+  // it's an updated worker and waiting.
+  self.skipWaiting();
+};
 
-class SwController {
-  static setProjectContent(fileRecords: FileRecord[], sessionId: string) {
-    const fileMap = new Map<string, ResponseParams>();
-    fileResponseMap.set(sessionId, fileMap);
+const onActivate = (event: ExtendableEvent) => {
+  // Make sure active clients use this service worker instance without being
+  // reloaded.
+  event.waitUntil(self.clients.claim());
+};
 
-    for (const fileRecord of fileRecords) {
-      let contentType = '';
-
-      switch (fileRecord.extension) {
-        case 'html':
-          contentType = 'text/html; charset=UTF-8';
-          break;
-        case 'js':
-          contentType = 'application/javascript; charset=UTF-8';
-          break;
-        default:
-          continue;
-      }
-
-      let responseInit: ResponseInit = {
-        headers: {
-          'Content-Type': contentType,
-        },
-      };
-
-      const filename = `${fileRecord.name}.${fileRecord.extension}`;
-      const responseParams: ResponseParams = {
-        content: fileRecord.content,
-        init: responseInit,
-      };
-
-      fileMap.set(filename, responseParams);
-    }
-  }
-
-  static clearContents(sessionId: string) {
-    fileResponseMap.delete(sessionId);
-  }
-
-  static get scope(): string {
-    return swScope.registration.scope;
-  }
-}
-
-const onMessage = (e: MessageEvent) => {
-  const data: Message = e.data;
-
-  switch (data.type) {
-    case MESSAGE_TYPES.ESTABLISH_HANDSHAKE:
-      recieveMessageChannelHandshake(e);
-      break;
+const onMessage = (e: ExtendableMessageEvent) => {
+  // Receive a handshake message from a page and setup Comlink.
+  if (e.data.initComlink === ESTABLISH_HANDSHAKE) {
+    (e.data.port as MessagePort).postMessage({
+      initComlink: HANDSHAKE_RECEIVED,
+    });
+    expose(workerAPI, e.data.port);
   }
 };
 
+self.addEventListener('fetch', onFetch);
+self.addEventListener('activate', onActivate);
+self.addEventListener('install', onInstall);
 self.addEventListener('message', onMessage);
