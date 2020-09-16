ab7c7ef06f2728541ab6a650fe3c0cf572fa6158
Elliott Marquez <emarquez@google.com>
2020-09-15 19:54:23 -0700

    docs(replacement): demo

---
+++ demo/replacements/my-second-element.js
@@ -0,0 +1,7 @@
+import {LitElement, html} from 'https://unpkg.com/lit-element?module';
+
+class MySecondElement extends LitElement {
+  /* second element render */
+}
+
+customElements.define('my-second-element', MySecondElement);
---
+++ demo/replacements/second-element-render.js
@@ -0,0 +1,5 @@
+render () {
+  return html`
+    <div>This is inside the shadow dom of my-second-element</div>
+  `;
+}
