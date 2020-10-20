7ac58b7f3f22f2e459e8b55b08dc0d40ba944a56
Alexander Marks <aomarks@google.com>
2020-10-20 13:56:03 -0700

    Simplify configurator example project

--- configurator/project/my-element.ts
+++ configurator/project/my-element.ts
@@ -2,14 +2,10 @@ import {LitElement, html, property, customElement} from 'lit-element';
 
 @customElement('my-element')
 export class MyElement extends LitElement {
-  @property({type: Number})
-  myNumber?: number;
+  @property()
+  greet = 'nobody';
 
   render() {
-    return html`
-      <div>Here is ${this.myNumber}</div>
-      <div>And here is my second element:</div>
-      <my-second-element></my-second-element>
-    `;
+    return html`<p>Hello <b>${this.greet}</b>!</p>`;
   }
 }
