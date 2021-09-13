7086de51ee65b19fc6d18e8780c07bbd57d5c052
Alexander Marks <aomarks@google.com>
2021-09-13 11:55:24 -0700

    Add NodeModuleResolver class

---
+++ src/test/node-module-resolver_test.ts
@@ -0,0 +1,246 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import {assert} from '@esm-bundle/chai';
+import {NodeModuleResolver} from '../typescript-worker/node-module-resolver.js';
+
+import type {PackageJson} from '../typescript-worker/util.js';
+
+suite('NodeModuleResolver', () => {
+  const check = ({
+    path,
+    packageJson,
+    conditions = ['default', 'module', 'import'],
+    expected,
+  }: {
+    path: string;
+    packageJson: PackageJson;
+    conditions?: string[];
+    expected: string;
+  }) => {
+    const resolver = new NodeModuleResolver({conditions});
+    const actual = resolver.resolve(
+      {pkg: 'mydep', version: '1.2.3', path},
+      packageJson,
+      './my-element.js'
+    );
+    assert.equal(actual, expected);
+  };
+
+  test('default with path', () => {
+    check({
+      path: 'foo.js',
+      packageJson: {},
+      expected: 'foo.js',
+    });
+  });
+
+  test('default no path', () => {
+    check({
+      path: '',
+      packageJson: {},
+      expected: 'index.js',
+    });
+  });
+
+  test('default with main', () => {
+    check({
+      path: '',
+      packageJson: {
+        main: 'main.js',
+      },
+      expected: 'main.js',
+    });
+  });
+
+  test('exports default module', () => {
+    check({
+      path: '',
+      packageJson: {
+        exports: {
+          '.': './main.js',
+        },
+      },
+      expected: 'main.js',
+    });
+  });
+
+  test('exports main sugar', () => {
+    check({
+      path: '',
+      packageJson: {
+        exports: './main.js',
+      },
+      expected: 'main.js',
+    });
+  });
+
+  test('exports remapped module', () => {
+    check({
+      path: 'foo.js',
+      packageJson: {
+        exports: {
+          './foo.js': './lib/foo.js',
+        },
+      },
+      expected: 'lib/foo.js',
+    });
+  });
+
+  test('throws if exports but no mapping', () => {
+    assert.throws(() => {
+      check({
+        path: 'foo.js',
+        packageJson: {
+          exports: {
+            './bar.js': './lib/bar.js',
+          },
+        },
+        expected: 'Package subpath',
+      });
+    }, 'Package subpath \'./foo.js\' is not defined by "exports" in /node_modules/mydep@1.2.3/package.json imported from ./my-element.js');
+  });
+
+  test("throws if target doesn't start with ./", () => {
+    assert.throws(() => {
+      check({
+        path: 'foo.js',
+        packageJson: {
+          exports: {
+            './foo.js': 'dist/foo.js',
+          },
+        },
+        expected: '',
+      });
+    }, 'Invalid "exports" target "dist/foo.js" defined for \'./foo.js\' in the package config /node_modules/mydep@1.2.3/package.json imported from ./my-element.js; targets must start with "./"');
+  });
+
+  test('subpath export', () => {
+    check({
+      path: 'features/foo.js',
+      packageJson: {
+        exports: {
+          './features/*': './src/features/*',
+        },
+      },
+      expected: 'src/features/foo.js',
+    });
+  });
+
+  test('banned subpath export', () => {
+    assert.throws(() => {
+      check({
+        path: 'features/private/foo.js',
+        packageJson: {
+          exports: {
+            './features/*': './src/features/*',
+            './features/private/*': null,
+          },
+        },
+        expected: '',
+      });
+    }, 'Package subpath \'./features/private/foo.js\' is not defined by "exports" in /node_modules/mydep@1.2.3/package.json imported from ./my-element.js');
+  });
+
+  test('conditions', () => {
+    check({
+      path: '',
+      packageJson: {
+        exports: {
+          '.': {
+            import: './main.js',
+            require: './main.cjs',
+          },
+        },
+      },
+      expected: 'main.js',
+    });
+  });
+
+  test('conditions with main sugar', () => {
+    check({
+      path: '',
+      packageJson: {
+        exports: {
+          import: './main.js',
+          require: './main.cjs',
+        },
+      },
+      expected: 'main.js',
+    });
+  });
+
+  test('condition precedence', () => {
+    check({
+      path: '',
+      packageJson: {
+        exports: {
+          '.': {
+            development: './dev.js',
+            default: './prod.js',
+          },
+        },
+      },
+      conditions: ['development'],
+      expected: 'dev.js',
+    });
+  });
+
+  test('nested conditions', () => {
+    const path = '';
+    const packageJson = {
+      exports: {
+        node: {
+          import: './feature-node.mjs',
+          require: './feature-node.cjs',
+        },
+        default: './feature.mjs',
+      },
+    };
+    check({
+      path,
+      packageJson,
+      conditions: ['browser'],
+      expected: 'feature.mjs',
+    });
+    check({
+      path,
+      packageJson,
+      conditions: ['node', 'import'],
+      expected: 'feature-node.mjs',
+    });
+    check({
+      path,
+      packageJson,
+      conditions: ['node', 'require'],
+      expected: 'feature-node.cjs',
+    });
+  });
+
+  test('target array, first is valid', () => {
+    check({
+      path: '',
+      packageJson: {
+        exports: {
+          '.': ['./main.js', 'INVALID'],
+        },
+      },
+      expected: 'main.js',
+    });
+  });
+
+  test('target array, second is valid', () => {
+    check({
+      path: '',
+      packageJson: {
+        exports: {
+          '.': ['INVALID', './main.js'],
+        },
+      },
+      expected: 'main.js',
+    });
+  });
+});
---
+++ src/typescript-worker/node-module-resolver.ts
@@ -0,0 +1,110 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import {packageExportsResolve} from './node/resolve.js';
+import type {
+  NpmFileLocation,
+  PackageJson,
+  PackageJsonWithExports,
+} from './util.js';
+
+/**
+ * Resolves a path according to the Node package exports algorithm.
+ *
+ * Documentation:
+ *
+ *   Node: https://nodejs.org/api/packages.html#packages_package_entry_points
+ *   Rollup: https://github.com/rollup/plugins/tree/master/packages/node-resolve/#package-entrypoints
+ *   Webpack: https://webpack.js.org/guides/package-exports/
+ *
+ * Reference implementations:
+ *
+ *   Node: https://github.com/nodejs/node/blob/a9dd03b1ec89a75186f05967fc76ec0704050c36/lib/internal/modules/esm/resolve.js#L615
+ *   Rollup:
+ * https://github.com/rollup/plugins/blob/53fb18c0c2852598200c547a0b1d745d15b5b487/packages/node-resolve/src/package/resolvePackageImportsExports.js#L6
+ */
+export class NodeModuleResolver {
+  private readonly _conditions: Set<string>;
+
+  constructor({conditions}: {conditions: string[]}) {
+    this._conditions = new Set(conditions);
+  }
+
+  /**
+   * @param location Package/version/path to resolve.
+   * @param packageJson The package's package.json (parsed object, not string).
+   * @param base Path of the importing module, used for error messages (e.g.
+   * "./my-element.js").
+   * @return The resolved subpath.
+   * @throws If the given subpath could not be resolved.
+   */
+  resolve(
+    location: NpmFileLocation,
+    packageJson: PackageJson,
+    base: string
+  ): string {
+    const packageSubpath = addRelativePrefix(location.path);
+
+    if (packageJson.exports === undefined) {
+      if (packageSubpath === '.') {
+        if (packageJson.module !== undefined) {
+          return removeRelativePrefix(packageJson.module);
+        }
+        if (packageJson.main !== undefined) {
+          return removeRelativePrefix(packageJson.main);
+        }
+        return 'index.js';
+      }
+      return location.path;
+    }
+
+    // Node's resolve functions works with file:// URLs. It doesn't really
+    // matter what we use as the base, but let's make one that matches the
+    // Playground URL space for dependencies, so that errors make sense.
+    const packageBase = `/node_modules/${location.pkg}@${location.version}/`;
+    const packageJsonUrl = new URL(packageBase, 'file://');
+
+    const resolved = packageExportsResolve(
+      packageJsonUrl,
+      packageSubpath,
+      packageJson as PackageJsonWithExports,
+      base,
+      this._conditions
+    );
+    if (!resolved.pathname.startsWith(packageBase)) {
+      throw new Error(
+        `Unexpected error: ${resolved.pathname} expected to start with ${packageBase}`
+      );
+    }
+    return resolved.pathname.slice(packageBase.length);
+  }
+}
+
+/**
+ * Convert e.g. "" to "." and "foo.js" to "./foo.js".
+ */
+const addRelativePrefix = (path: string): string => {
+  if (path === '') {
+    return '.';
+  }
+  if (!path.startsWith('./')) {
+    return './' + path;
+  }
+  return path;
+};
+
+/**
+ * Convert e.g. "." to "" and "./foo.js" to "foo.js".
+ */
+const removeRelativePrefix = (path: string): string => {
+  if (path === '.') {
+    return '';
+  }
+  if (path.startsWith('./')) {
+    return path.slice(2);
+  }
+  return path;
+};
