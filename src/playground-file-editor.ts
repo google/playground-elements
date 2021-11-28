6fd9b8c20b837d418231ddd86291f10975c515e2
Matsuuu <huhta.matias@gmail.com>
2021-11-28 14:07:42 +0200

    Hide completions after a complete code action

--- src/playground-file-editor.ts
+++ src/playground-file-editor.ts
@@ -4,21 +4,22 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import {html, css, PropertyValues} from 'lit';
-import {customElement, property, query} from 'lit/decorators.js';
-import {live} from 'lit/directives/live.js';
+import { html, css, PropertyValues } from 'lit';
+import { customElement, property, query } from 'lit/decorators.js';
+import { live } from 'lit/directives/live.js';
 
 import './playground-code-editor.js';
-import {PlaygroundProject} from './playground-project.js';
-import {PlaygroundCodeEditor} from './playground-code-editor.js';
-import {PlaygroundConnectedElement} from './playground-connected-element.js';
+import { PlaygroundProject } from './playground-project.js';
+import { PlaygroundCodeEditor } from './playground-code-editor.js';
+import { PlaygroundConnectedElement } from './playground-connected-element.js';
+import type { EditorChange } from 'codemirror';
 
 /**
  * A text editor associated with a <playground-project>.
  */
 @customElement('playground-file-editor')
 export class PlaygroundFileEditor extends PlaygroundConnectedElement {
-  static styles = css`
+    static styles = css`
     :host {
       display: block;
       /* Prevents scrollbars from changing container size and shifting layout
@@ -41,193 +42,196 @@ export class PlaygroundFileEditor extends PlaygroundConnectedElement {
     }
   `;
 
-  @query('playground-code-editor')
-  private _editor!: PlaygroundCodeEditor;
+    @query('playground-code-editor')
+    private _editor!: PlaygroundCodeEditor;
 
-  /**
-   * The name of the project file that is currently being displayed.
-   */
-  @property()
-  filename?: string;
+    /**
+     * The name of the project file that is currently being displayed.
+     */
+    @property()
+    filename?: string;
 
-  /**
-   * If true, display a left-hand-side gutter with line numbers. Default false
-   * (hidden).
-   */
-  @property({type: Boolean, attribute: 'line-numbers'})
-  lineNumbers = false;
+    /**
+     * If true, display a left-hand-side gutter with line numbers. Default false
+     * (hidden).
+     */
+    @property({ type: Boolean, attribute: 'line-numbers' })
+    lineNumbers = false;
 
-  /**
-   * How to handle `playground-hide` and `playground-fold` comments.
-   *
-   * See https://github.com/google/playground-elements#hiding--folding for
-   * more details.
-   *
-   * Options:
-   * - on: Hide and fold regions, and hide the special comments.
-   * - off: Don't hide or fold regions, but still hide the special comments.
-   * - off-visible: Don't hide or fold regions, and show the special comments as
-   *   literal text.
-   */
-  @property()
-  pragmas: 'on' | 'off' | 'off-visible' = 'on';
+    /**
+     * How to handle `playground-hide` and `playground-fold` comments.
+     *
+     * See https://github.com/google/playground-elements#hiding--folding for
+     * more details.
+     *
+     * Options:
+     * - on: Hide and fold regions, and hide the special comments.
+     * - off: Don't hide or fold regions, but still hide the special comments.
+     * - off-visible: Don't hide or fold regions, and show the special comments as
+     *   literal text.
+     */
+    @property()
+    pragmas: 'on' | 'off' | 'off-visible' = 'on';
 
-  /**
-   * If true, this editor is not editable.
-   */
-  @property({type: Boolean, reflect: true})
-  readonly = false;
+    /**
+     * If true, this editor is not editable.
+     */
+    @property({ type: Boolean, reflect: true })
+    readonly = false;
 
-  private get _files() {
-    return this._project?.files ?? [];
-  }
-
-  private get _currentFile() {
-    return this.filename
-      ? this._files.find((file) => file.name === this.filename)
-      : undefined;
-  }
-
-  async update(changedProperties: PropertyValues) {
-    if (changedProperties.has('_project')) {
-      const oldProject = changedProperties.get('_project') as PlaygroundProject;
-      if (oldProject) {
-        oldProject.removeEventListener(
-          'filesChanged',
-          this._onProjectFilesChanged
-        );
-        oldProject.removeEventListener('compileDone', this._onCompileDone);
-        oldProject.removeEventListener(
-          'diagnosticsChanged',
-          this._onDiagnosticsChanged
-        );
-        oldProject.removeEventListener(
-          'completionsChanged',
-          this._onCompletionsChanged
-        );
-      }
-      if (this._project) {
-        this._project.addEventListener(
-          'filesChanged',
-          this._onProjectFilesChanged
-        );
-        this._project.addEventListener('compileDone', this._onCompileDone);
-        this._project.addEventListener(
-          'diagnosticsChanged',
-          this._onDiagnosticsChanged
-        );
-        this._project.addEventListener(
-          'completionsChanged',
-          this._onCompletionsChanged
-        );
-      }
-      this._onProjectFilesChanged();
+    private get _files() {
+        return this._project?.files ?? [];
     }
-    super.update(changedProperties);
-  }
 
-  render() {
-    return html`
+    private get _currentFile() {
+        return this.filename
+            ? this._files.find((file) => file.name === this.filename)
+            : undefined;
+    }
+
+    async update(changedProperties: PropertyValues) {
+        if (changedProperties.has('_project')) {
+            const oldProject = changedProperties.get('_project') as PlaygroundProject;
+            if (oldProject) {
+                oldProject.removeEventListener(
+                    'filesChanged',
+                    this._onProjectFilesChanged
+                );
+                oldProject.removeEventListener('compileDone', this._onCompileDone);
+                oldProject.removeEventListener(
+                    'diagnosticsChanged',
+                    this._onDiagnosticsChanged
+                );
+                oldProject.removeEventListener(
+                    'completionsChanged',
+                    this._onCompletionsChanged
+                );
+            }
+            if (this._project) {
+                this._project.addEventListener(
+                    'filesChanged',
+                    this._onProjectFilesChanged
+                );
+                this._project.addEventListener('compileDone', this._onCompileDone);
+                this._project.addEventListener(
+                    'diagnosticsChanged',
+                    this._onDiagnosticsChanged
+                );
+                this._project.addEventListener(
+                    'completionsChanged',
+                    this._onCompletionsChanged
+                );
+            }
+            this._onProjectFilesChanged();
+        }
+        super.update(changedProperties);
+    }
+
+    render() {
+        return html`
       ${this._files
-        ? html`
+                ? html`
             <playground-code-editor
               exportparts="diagnostic-tooltip, dialog"
               .value=${
-                // We need live() because the lit's dirty-checking value for
-                // content is not updated by user edits.
-                live(this._currentFile?.content ?? '')
-              }
+                    // We need live() because the lit's dirty-checking value for
+                    // content is not updated by user edits.
+                    live(this._currentFile?.content ?? '')
+                    }
               .type=${this._currentFile
-                ? mimeTypeToTypeEnum(this._currentFile.contentType)
-                : undefined}
+                        ? mimeTypeToTypeEnum(this._currentFile.contentType)
+                        : undefined}
               .lineNumbers=${this.lineNumbers}
               .readonly=${this.readonly || !this._currentFile}
               .pragmas=${this.pragmas}
               .diagnostics=${this._project?.diagnostics?.get(
-                this._currentFile?.name ?? ''
-              )}
+                            this._currentFile?.name ?? ''
+                        )}
               .completions=${this._project?.completions}
               .completionItemDetails=${this._project?.completionItemDetails}
               @change=${this._onEdit}
             >
             </playground-code-editor>
           `
-        : html`<slot></slot>`}
+                : html`<slot></slot>`}
     `;
-  }
-
-  private _onProjectFilesChanged = () => {
-    this.filename ??= this._files[0]?.name;
-    this.requestUpdate();
-  };
-
-  private _onCompileDone = () => {
-    // Propagate diagnostics.
-    this.requestUpdate();
-  };
-
-  private _onDiagnosticsChanged = () => {
-    // Propagate diagnostics.
-    this.requestUpdate();
-  };
-
-  private _onCompletionsChanged = () => {
-    // Propagate completions.
-    this.requestUpdate();
-  };
-
-  private _completionTrigger() {
-    this._project?.getCompletions(
-      this.filename ?? '',
-      this._editor?.value ?? '',
-      this._editor.tokenUnderCursor,
-      this._editor.cursorIndex
-    );
-  }
-
-  private _onEdit() {
-    if (
-      this._project === undefined ||
-      this._currentFile === undefined ||
-      this._editor.value === undefined
-    ) {
-      return;
     }
-    this._completionTrigger();
-    this._project.editFile(this._currentFile, this._editor.value);
-  }
+
+    private _onProjectFilesChanged = () => {
+        this.filename ??= this._files[0]?.name;
+        this.requestUpdate();
+    };
+
+    private _onCompileDone = () => {
+        // Propagate diagnostics.
+        this.requestUpdate();
+    };
+
+    private _onDiagnosticsChanged = () => {
+        // Propagate diagnostics.
+        this.requestUpdate();
+    };
+
+    private _onCompletionsChanged = () => {
+        // Propagate completions.
+        this.requestUpdate();
+    };
+
+    private _completionTrigger(changeObject: EditorChange) {
+        this._project?.getCompletions(
+            this.filename ?? '',
+            this._editor?.value ?? '',
+            this._editor.tokenUnderCursor,
+            this._editor.cursorIndex,
+            changeObject
+        );
+    }
+
+    private _onEdit(e: CustomEvent) {
+        if (
+            this._project === undefined ||
+            this._currentFile === undefined ||
+            this._editor.value === undefined
+        ) {
+            return;
+        }
+        const changeObject = e.detail.changeObject as EditorChange;
+        console.log(changeObject);
+        this._completionTrigger(changeObject);
+        this._project.editFile(this._currentFile, this._editor.value);
+    }
 }
 
 declare global {
-  interface HTMLElementTagNameMap {
-    'playground-file-editor': PlaygroundFileEditor;
-  }
+    interface HTMLElementTagNameMap {
+        'playground-file-editor': PlaygroundFileEditor;
+    }
 }
 
 const mimeTypeToTypeEnum = (mimeType?: string) => {
-  // TODO: infer type based on extension too
-  if (mimeType === undefined) {
-    return;
-  }
-  const encodingSepIndex = mimeType.indexOf(';');
-  if (encodingSepIndex !== -1) {
-    mimeType = mimeType.substring(0, encodingSepIndex);
-  }
-  switch (mimeType) {
-    // TypeScript: this is the mime-type returned by servers
-    // .ts files aren't usually served to browsers, so they don't yet
-    // have their own mime-type.
-    case 'video/mp2t':
-      return 'ts';
-    case 'text/javascript':
-    case 'application/javascript':
-      return 'js';
-    case 'application/json':
-      return 'json';
-    case 'text/html':
-      return 'html';
-    case 'text/css':
-      return 'css';
-  }
-  return undefined;
+    // TODO: infer type based on extension too
+    if (mimeType === undefined) {
+        return;
+    }
+    const encodingSepIndex = mimeType.indexOf(';');
+    if (encodingSepIndex !== -1) {
+        mimeType = mimeType.substring(0, encodingSepIndex);
+    }
+    switch (mimeType) {
+        // TypeScript: this is the mime-type returned by servers
+        // .ts files aren't usually served to browsers, so they don't yet
+        // have their own mime-type.
+        case 'video/mp2t':
+            return 'ts';
+        case 'text/javascript':
+        case 'application/javascript':
+            return 'js';
+        case 'application/json':
+            return 'json';
+        case 'text/html':
+            return 'html';
+        case 'text/css':
+            return 'css';
+    }
+    return undefined;
 };
---
+++ src/shared/codemirror-values.ts
@@ -0,0 +1,5 @@
+export const EditorChangeOrigin = {
+    INSERT: "+insert",
+    DELETE: "+delete",
+    COMPLETE: "complete"
+}
