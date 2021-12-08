2257c640b011a4e2f8c4bd1afd3724be6527e769
Matsuuu <huhta.matias@gmail.com>
2021-12-08 19:14:17 +0200

    Remove unnecessary updateFile function from LS context

--- src/typescript-worker/completions.ts
+++ src/typescript-worker/completions.ts
@@ -6,105 +6,106 @@
 
 import Fuse from 'fuse.js';
 import type {
-  GetCompletionsAtPositionOptions,
-  SymbolDisplayPart,
+    GetCompletionsAtPositionOptions,
+    SymbolDisplayPart,
 } from 'typescript';
 import {
-  EditorCompletion,
-  EditorCompletionDetails,
-  EditorCompletionMatch,
-  EditorToken,
-  WorkerConfig,
+    EditorCompletion,
+    EditorCompletionDetails,
+    EditorCompletionMatch,
+    EditorToken,
+    WorkerConfig,
 } from '../shared/worker-api.js';
-import {getWorkerContext} from './worker-context.js';
+import { getWorkerContext } from './worker-context.js';
 
 /**
  * Query completions from the Language Service, and sort them by
  * relevance for user to use.
  * */
 export const queryCompletions = async (
-  filename: string,
-  fileContent: string,
-  tokenUnderCursor: EditorToken,
-  cursorIndex: number,
-  config: WorkerConfig
+    filename: string,
+    fileContent: string,
+    tokenUnderCursor: EditorToken,
+    cursorIndex: number,
+    config: WorkerConfig
 ): Promise<EditorCompletion[]> => {
-  const workerContext = getWorkerContext(config);
+    const workerContext = getWorkerContext(config);
 
-  const languageService = workerContext.languageServiceContext.service;
-  const languageServiceHost = workerContext.languageServiceContext.serviceHost;
-  const searchWordIsPeriod = tokenUnderCursor.string === '.';
+    const languageService = workerContext.languageServiceContext.service;
+    const languageServiceHost = workerContext.languageServiceContext.serviceHost;
+    const searchWordIsPeriod = tokenUnderCursor.string === '.';
 
-  const options = {} as GetCompletionsAtPositionOptions;
-  if (searchWordIsPeriod) {
-    options.triggerCharacter = '.';
-  }
+    const options = {} as GetCompletionsAtPositionOptions;
+    if (searchWordIsPeriod) {
+        options.triggerCharacter = '.';
+    }
 
-  // Update language service status so that the file is up to date
-  languageServiceHost.updateFile(filename, fileContent);
+    // Update language service status so that the file is up to date
+    const fileAbsolutePath = new URL(filename, self.origin).href;
+    languageServiceHost.updateFileContentIfNeeded(fileAbsolutePath, fileContent);
 
-  // Fetch the collection of completions, the language service offers us for our current context.
-  // This list of completions is quite vast, and therefore we will need to do some extra sorting
-  // and filtering on it before sending it back to the browser.
-  const completions = languageService.getCompletionsAtPosition(
-    filename,
-    cursorIndex,
-    options
-  );
+    // Fetch the collection of completions, the language service offers us for our current context.
+    // This list of completions is quite vast, and therefore we will need to do some extra sorting
+    // and filtering on it before sending it back to the browser.
+    const completions = languageService.getCompletionsAtPosition(
+        filename,
+        cursorIndex,
+        options
+    );
 
-  if (searchWordIsPeriod) {
-    // On period, just return the completions without fuzzy finding since there's no need for it
-    const editorCompletions =
-      completions?.entries
-        .sort((a, b) => parseInt(a.sortText) - parseInt(b.sortText))
-        .map((comp) => ({
-          // Since the completion engine will only append the word
-          // given as the text property here, auto-completing from a period
-          // would replace the period with the word. This is why we need
-          // to append the priod into the text property. This is not visible to the
-          // user however, so no harm is done.
-          text: '.' + comp.name,
-          displayText: comp.name,
-          score: parseInt(comp.sortText),
-        })) ?? [];
+    if (searchWordIsPeriod) {
+        // On period, just return the completions without fuzzy finding since there's no need for it
+        const editorCompletions =
+            completions?.entries
+                .sort((a, b) => parseInt(a.sortText) - parseInt(b.sortText))
+                .map((comp) => ({
+                    // Since the completion engine will only append the word
+                    // given as the text property here, auto-completing from a period
+                    // would replace the period with the word. This is why we need
+                    // to append the priod into the text property. This is not visible to the
+                    // user however, so no harm is done.
+                    text: '.' + comp.name,
+                    displayText: comp.name,
+                    score: parseInt(comp.sortText),
+                })) ?? [];
 
-    return editorCompletions;
-  }
-  // If the user input a letter or a partial word, we want to offer
-  // the closest matches first, and the weaker matches after. We will use
-  // Fuse to score our completions by their fuzzy matches.
-  const fuse = new Fuse(completions?.entries ?? [], {
-    threshold: 0.3,
-    distance: 20,
-    shouldSort: true,
-    isCaseSensitive: true,
-    includeScore: true,
-    includeMatches: true,
-    findAllMatches: true,
-    keys: ['name'],
-    minMatchCharLength: Math.max(tokenUnderCursor.string.length, 1),
-  });
-  const relevantCompletions = fuse.search(tokenUnderCursor.string);
-
-  const editorCompletions: EditorCompletion[] = relevantCompletions
-    // Map the relevant info from fuse scoring
-    .map((item) => ({
-      text: item.item.name,
-      displayText: item.item.name,
-      score: item.score ?? 0,
-      matches: item.matches as EditorCompletionMatch[],
-    }))
-    // Sort the completions by how well they matched the given keyword
-    .sort((a, b) => {
-      if (a.score === b.score) {
-        return a.text.localeCompare(b.text);
-      }
-      return a.score - b.score;
+        return editorCompletions;
+    }
+    // If the user input a letter or a partial word, we want to offer
+    // the closest matches first, and the weaker matches after. We will use
+    // Fuse to score our completions by their fuzzy matches.
+    const fuse = new Fuse(completions?.entries ?? [], {
+        threshold: 0.3,
+        distance: 20,
+        shouldSort: true,
+        isCaseSensitive: true,
+        includeScore: true,
+        includeMatches: true,
+        findAllMatches: true,
+        keys: ['name'],
+        minMatchCharLength: Math.max(tokenUnderCursor.string.length, 1),
     });
+    const relevantCompletions = fuse.search(tokenUnderCursor.string);
 
-  // Limit to a 100 completion suggestions to save some bandwidth
-  // ...Surely no-one wants more than a 100 suggestions?
-  return editorCompletions.slice(0, 100);
+    const editorCompletions: EditorCompletion[] = relevantCompletions
+        // Map the relevant info from fuse scoring
+        .map((item) => ({
+            text: item.item.name,
+            displayText: item.item.name,
+            score: item.score ?? 0,
+            matches: item.matches as EditorCompletionMatch[],
+        }))
+        // Sort the completions by how well they matched the given keyword
+        .sort((a, b) => {
+            if (a.score === b.score) {
+                return a.text.localeCompare(b.text);
+            }
+            return a.score - b.score;
+        });
+
+    // Limit to a 100 completion suggestions to save some bandwidth
+    // ...Surely no-one wants more than a 100 suggestions?
+    return editorCompletions.slice(0, 100);
 };
 
 /**
@@ -112,54 +113,54 @@ export const queryCompletions = async (
  * context and signatures.
  * */
 export const getCompletionItemDetails = async (
-  filename: string,
-  cursorIndex: number,
-  config: WorkerConfig,
-  completionWord: string
+    filename: string,
+    cursorIndex: number,
+    config: WorkerConfig,
+    completionWord: string
 ): Promise<EditorCompletionDetails> => {
-  const workerContext = getWorkerContext(config);
-  const languageService = workerContext.languageServiceContext.service;
+    const workerContext = getWorkerContext(config);
+    const languageService = workerContext.languageServiceContext.service;
 
-  // Only passing relevant params for now, since the other values
-  // are not needed for current functionality
-  const details = languageService.getCompletionEntryDetails(
-    filename,
-    cursorIndex,
-    completionWord,
-    undefined,
-    undefined,
-    undefined,
-    undefined
-  );
+    // Only passing relevant params for now, since the other values
+    // are not needed for current functionality
+    const details = languageService.getCompletionEntryDetails(
+        filename,
+        cursorIndex,
+        completionWord,
+        undefined,
+        undefined,
+        undefined,
+        undefined
+    );
 
-  const detailInformation: EditorCompletionDetails = {
-    text: displayPartsToString(details?.displayParts),
-    tags: details?.tags ?? [],
-    documentation: getDocumentations(details?.documentation),
-  };
-  return detailInformation;
+    const detailInformation: EditorCompletionDetails = {
+        text: displayPartsToString(details?.displayParts),
+        tags: details?.tags ?? [],
+        documentation: getDocumentations(details?.documentation),
+    };
+    return detailInformation;
 };
 
 function displayPartsToString(
-  displayParts: SymbolDisplayPart[] | undefined
+    displayParts: SymbolDisplayPart[] | undefined
 ): string {
-  if (!displayParts || displayParts.length === 0) return '';
+    if (!displayParts || displayParts.length === 0) return '';
 
-  let displayString = '';
-  displayParts.forEach((part) => {
-    displayString += part.text;
-  });
-  return displayString;
+    let displayString = '';
+    displayParts.forEach((part) => {
+        displayString += part.text;
+    });
+    return displayString;
 }
 
 function getDocumentations(
-  documentation: SymbolDisplayPart[] | undefined
+    documentation: SymbolDisplayPart[] | undefined
 ): string[] {
-  if (!documentation || documentation.length === 0) return [];
+    if (!documentation || documentation.length === 0) return [];
 
-  const documentationStrings: string[] = [];
-  documentation.forEach((doc) => {
-    documentationStrings.push(doc.text);
-  });
-  return documentationStrings;
+    const documentationStrings: string[] = [];
+    documentation.forEach((doc) => {
+        documentationStrings.push(doc.text);
+    });
+    return documentationStrings;
 }
--- src/typescript-worker/language-service-context.ts
+++ src/typescript-worker/language-service-context.ts
@@ -7,13 +7,13 @@
 import ts from '../internal/typescript.js';
 
 const compilerOptions = {
-  target: ts.ScriptTarget.ES2017,
-  module: ts.ModuleKind.ESNext,
-  experimentalDecorators: true,
-  skipDefaultLibCheck: true,
-  skipLibCheck: true,
-  moduleResolution: ts.ModuleResolutionKind.NodeJs,
-  lib: ['dom', 'esnext'],
+    target: ts.ScriptTarget.ES2017,
+    module: ts.ModuleKind.ESNext,
+    experimentalDecorators: true,
+    skipDefaultLibCheck: true,
+    skipLibCheck: true,
+    moduleResolution: ts.ModuleResolutionKind.NodeJs,
+    lib: ['dom', 'esnext'],
 };
 
 /**
@@ -25,117 +25,106 @@ const compilerOptions = {
  * common lib files like lit-element, lib.d.ts and dom.d.ts.
  */
 export class LanguageServiceContext {
-  readonly compilerOptions = compilerOptions;
+    readonly compilerOptions = compilerOptions;
 
-  readonly serviceHost = new WorkerLanguageServiceHost(
-    self.origin,
-    compilerOptions
-  );
+    readonly serviceHost = new WorkerLanguageServiceHost(
+        self.origin,
+        compilerOptions
+    );
 
-  readonly service = ts.createLanguageService(
-    this.serviceHost,
-    ts.createDocumentRegistry()
-  );
+    readonly service = ts.createLanguageService(
+        this.serviceHost,
+        ts.createDocumentRegistry()
+    );
 }
 
 interface VersionedFile {
-  version: number;
-  content: string;
+    version: number;
+    content: string;
 }
 
 class WorkerLanguageServiceHost implements ts.LanguageServiceHost {
-  readonly compilerOptions: ts.CompilerOptions;
-  readonly packageRoot: string;
-  readonly files: Map<string, VersionedFile> = new Map<string, VersionedFile>();
+    readonly compilerOptions: ts.CompilerOptions;
+    readonly packageRoot: string;
+    readonly files: Map<string, VersionedFile> = new Map<string, VersionedFile>();
 
-  constructor(packageRoot: string, compilerOptions: ts.CompilerOptions) {
-    this.packageRoot = packageRoot;
-    this.compilerOptions = compilerOptions;
-  }
-
-  /*
-   *  When a new new "process" command is received, we iterate through all of the files,
-   *  and update files accordingly depending on if they have new content or not.
-   *
-   *  With how the TS API works, we can use simple versioning to tell the
-   *  Language service that a file has been updated
-   *
-   *  If the file submitted is a new file, we add it to our collection
-   */
-  updateFileContentIfNeeded(fileName: string, content: string) {
-    const file = this.files.get(fileName);
-    if (file && file.content !== content) {
-      file.content = content;
-      file.version += 1;
-    } else {
-      this.files.set(fileName, {content, version: 0});
+    constructor(packageRoot: string, compilerOptions: ts.CompilerOptions) {
+        this.packageRoot = packageRoot;
+        this.compilerOptions = compilerOptions;
     }
-  }
 
-  /**
-   * Sync up the freshly acquired project files.
-   * In the syncing process files yet to be added are added, and versioned.
-   * Files that existed already but are modified are updated, and their version number
-   * gets bumped fo that the languageservice knows to update these files.
-   * */
-  sync(files: Map<string, string>) {
-    files.forEach((file, fileName) =>
-      this.updateFileContentIfNeeded(fileName, file)
-    );
-    this._removeDeletedFiles(files);
-  }
-
-  updateFile(filename: string, fileContent: string) {
-    const fileAbsolutePath = new URL(filename, self.origin).href;
-    const currentFile = this.files.get(fileAbsolutePath);
-    if (currentFile) {
-      currentFile.version += 1;
-      currentFile.content = fileContent;
-    } else {
-      this.files.set(fileAbsolutePath, {content: fileContent, version: 0});
+    /*
+     *  When a new new "process" command is received, we iterate through all of the files,
+     *  and update files accordingly depending on if they have new content or not.
+     *
+     *  With how the TS API works, we can use simple versioning to tell the
+     *  Language service that a file has been updated
+     *
+     *  If the file submitted is a new file, we add it to our collection
+     */
+    updateFileContentIfNeeded(fileName: string, content: string) {
+        const file = this.files.get(fileName);
+        if (file && file.content !== content) {
+            file.content = content;
+            file.version += 1;
+        } else {
+            this.files.set(fileName, { content, version: 0 });
+        }
     }
-  }
 
-  private _removeDeletedFiles(files: Map<string, string>) {
-    this.getScriptFileNames().forEach((fileName) => {
-      if (!files.has(fileName)) {
-        this.files.delete(fileName);
-      }
-    });
-  }
-
-  getCompilationSettings(): ts.CompilerOptions {
-    return this.compilerOptions;
-  }
-
-  getScriptFileNames(): string[] {
-    return [...this.files.keys()];
-  }
-
-  getScriptVersion(fileName: string) {
-    return this.files.get(fileName)?.version.toString() ?? '-1';
-  }
-
-  fileExists(fileName: string): boolean {
-    return this.files.has(fileName);
-  }
-
-  readFile(fileName: string): string | undefined {
-    return this.files.get(fileName)?.content;
-  }
-
-  getScriptSnapshot(fileName: string): ts.IScriptSnapshot | undefined {
-    if (!this.fileExists(fileName)) {
-      return undefined;
+    /**
+     * Sync up the freshly acquired project files.
+     * In the syncing process files yet to be added are added, and versioned.
+     * Files that existed already but are modified are updated, and their version number
+     * gets bumped fo that the languageservice knows to update these files.
+     * */
+    sync(files: Map<string, string>) {
+        files.forEach((file, fileName) =>
+            this.updateFileContentIfNeeded(fileName, file)
+        );
+        this._removeDeletedFiles(files);
     }
-    return ts.ScriptSnapshot.fromString(this.readFile(fileName)!);
-  }
 
-  getCurrentDirectory(): string {
-    return this.packageRoot;
-  }
+    private _removeDeletedFiles(files: Map<string, string>) {
+        this.getScriptFileNames().forEach((fileName) => {
+            if (!files.has(fileName)) {
+                this.files.delete(fileName);
+            }
+        });
+    }
 
-  getDefaultLibFileName(): string {
-    return '__lib.d.ts';
-  }
+    getCompilationSettings(): ts.CompilerOptions {
+        return this.compilerOptions;
+    }
+
+    getScriptFileNames(): string[] {
+        return [...this.files.keys()];
+    }
+
+    getScriptVersion(fileName: string) {
+        return this.files.get(fileName)?.version.toString() ?? '-1';
+    }
+
+    fileExists(fileName: string): boolean {
+        return this.files.has(fileName);
+    }
+
+    readFile(fileName: string): string | undefined {
+        return this.files.get(fileName)?.content;
+    }
+
+    getScriptSnapshot(fileName: string): ts.IScriptSnapshot | undefined {
+        if (!this.fileExists(fileName)) {
+            return undefined;
+        }
+        return ts.ScriptSnapshot.fromString(this.readFile(fileName)!);
+    }
+
+    getCurrentDirectory(): string {
+        return this.packageRoot;
+    }
+
+    getDefaultLibFileName(): string {
+        return '__lib.d.ts';
+    }
 }
