339003db4f218a3cfb144e86ec96e584a500e6b5
Justin Poehnelt <jpoehnelt@google.com>
2022-03-04 09:35:38 -0700

    fix: ignore case in mimetype to type enum

--- src/playground-file-editor.ts
+++ src/playground-file-editor.ts
@@ -210,7 +210,7 @@ const mimeTypeToTypeEnum = (mimeType?: string) => {
   if (encodingSepIndex !== -1) {
     mimeType = mimeType.substring(0, encodingSepIndex);
   }
-  switch (mimeType) {
+  switch (mimeType.toLowerCase()) {
     // TypeScript: this is the mime-type returned by servers
     // .ts files aren't usually served to browsers, so they don't yet
     // have their own mime-type.
