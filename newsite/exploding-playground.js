de7cbf0b327c1ccbc48cb4bdbbaad1bb5d1fa4f3
Alexander Marks <aomarks@google.com>
2022-09-12 18:21:44 -0700

    More work on playground landing page

--- newsite/exploding-playground.js
+++ newsite/exploding-playground.js
@@ -4,51 +4,99 @@ import '../playground-project.js';
 import '../playground-file-editor.js';
 import '../playground-preview.js';
 import '../playground-tab-bar.js';
-import {classMap} from 'lit/directives/class-map.js';
+import {themeNames, themeStyles} from '../configurator/themes.js';
 
 class ExplodingPlayground extends LitElement {
-  static styles = css`
-    div {
-      display: grid;
-      grid-template-rows: 40px 300px;
-      grid-template-columns: 200px 400px;
-    }
-    :host([state='exploded']) > div {
-      grid-gap: 50px;
-    }
-    :host([state='vertical']) > div {
-      grid-template-rows: 40px 200px 50px;
-      grid-template-columns: 300px;
-    }
-    playground-tab-bar {
-      grid-column-start: 1;
-      grid-column-end: 3;
-    }
-    :host([state='vertical']) > div > playground-tab-bar {
-      grid-column-end: 1;
-    }
-    playground-file-editor {
-      border: 1px solid #ccc;
-    }
-    playground-preview {
-      border: 1px solid #ccc;
-    }
-    :host([state='vertical']) > div > playground-tab-bar {
-      opacity: 0;
-    }
-    :host {
-      font-family: Arial, sans-serif;
-      --playground-highlight-color: blue;
-    }
-  `;
+  static styles = [
+    ...themeStyles,
+    css`
+      div {
+        display: grid;
+        grid-template-rows: 40px 300px;
+        grid-template-columns: 300px 200px;
+        transform-origin: center;
+      }
+      :host([state='exploded']) > div {
+        grid-gap: 50px;
+      }
+      :host([state='flipped']) > div {
+        transform: rotateY(180deg);
+      }
+      :host > div > * {
+        box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px,
+          rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
+      }
+      :host([state='vertical']) > div {
+        grid-template-rows: 40px 200px 50px;
+        grid-template-columns: 300px;
+      }
+      playground-tab-bar {
+        grid-column-start: 1;
+        grid-column-end: 3;
+        border-radius: 5px 5px 0 0;
+      }
+      :host([state='vertical']) > div > playground-tab-bar {
+        grid-column-end: 1;
+      }
+      playground-file-editor {
+        border: 1px solid #ccc;
+      }
+      playground-preview {
+        border: 1px solid #ccc;
+      }
+      :host([state='vertical']) > div > playground-tab-bar {
+        opacity: 0;
+      }
+      :host {
+        font-family: Arial, sans-serif;
+        --playground-highlight-color: blue;
+      }
+      :host([state='themes']) > div {
+        grid-template-columns: 600px 0;
+      }
+      :host([state='themes']) > div > playground-tab-bar,
+      :host([state='themes']) > div > playground-preview {
+        opacity: 0;
+      }
+    `,
+  ];
 
   static properties = {
     state: {type: String, reflect: true},
+    theme: {type: String, reflect: true},
   };
 
   constructor() {
     super();
     this.state = 'initial';
+    this.theme = '';
+  }
+
+  update(changed) {
+    super.update(changed);
+    if (changed.has('state')) {
+      if (this.state === 'themes') {
+        this.startRotatingThemes();
+      } else {
+        this.stopRotatingThemes();
+      }
+    }
+  }
+
+  startRotatingThemes() {
+    this.stopRotatingThemes();
+    let i = 0;
+    this.rotateThemesTimer = setInterval(() => {
+      this.theme = themeNames[i++ % themeNames.length];
+    }, 500);
+  }
+
+  stopRotatingThemes() {
+    if (this.rotateThemesTimer !== undefined) {
+      clearInterval(this.rotateThemesTimer);
+      this.rotateThemesTimer = undefined;
+    }
+    this.theme = '';
   }
 
   render() {
@@ -66,14 +114,30 @@ class ExplodingPlayground extends LitElement {
           document.body.appendChild(document.createTextNode("World!"))
         </script>
       </playground-project>
-      <div>
+      <div
+        class="playground-theme-${this.theme}"
+        ${animate({
+          onFrames: (a) =>
+            a.frames &&
+            a.frames.map((f) => ({...f, transformOrigin: 'center center'})),
+          properties: [
+            'left',
+            'top',
+            'width',
+            'height',
+            'opacity',
+            'color',
+            'background',
+            'transform',
+          ],
+        })}
+      >
         <playground-tab-bar project="project" editor="editor" ${animate()}>
         </playground-tab-bar>
 
         <playground-file-editor
           id="editor"
           project="project"
-          ${animate()}
           filename="index.html"
         ></playground-file-editor>
 
