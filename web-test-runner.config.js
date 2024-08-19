0c35de2f92cc33e9d26af4ae2ce02e89e8b1af63
Justin Fagnani <justin@fagnani.com>
2024-08-19 13:19:42 -0700

    Remove puppeteer

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -5,7 +5,6 @@
  */
 
 import {playwrightLauncher} from '@web/test-runner-playwright';
-// import {puppeteerLauncher} from '@web/test-runner-puppeteer';
 import {fakeCdnPlugin} from './test/fake-cdn-plugin.js';
 import {startDevServer} from '@web/dev-server';
 
@@ -39,14 +38,6 @@ export default {
     playwrightLauncher({product: 'chromium'}),
     playwrightLauncher({product: 'webkit'}),
     playwrightLauncher({product: 'firefox'}),
-    // Playwright Firefox does not currently work with service workers, see
-    // https://github.com/microsoft/playwright/issues/7288.
-    //
-    // Also note we can't use Puppeteer for both Chromium and Firefox, because
-    // only one or the other can be installed at once (see our "postinstall" NPM
-    // script). See
-    // https://modern-web.dev/docs/test-runner/browser-launchers/puppeteer/.
-    // puppeteerLauncher({launchOptions: {product: 'firefox'}, concurrency: 1}),
   ],
   concurrentBrowsers: Number(process.env.CONCURRENT_BROWSERS) || 2, // default 2
   browserStartTimeout: 30000, // default 30000
